bdb25d5fdb4e2e0d3f451df215d84c45478e4b11
Oleh <50545689+chomupashchuk@users.noreply.github.com>
2020-07-09 15:17:11 +0300

    Add files via upload

---
+++ aristonremotethermo/__init__.py
@@ -0,0 +1,3 @@
+from . import ariston
+
+__all__ = ['ariston']
---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    README = readme_file.read()
+
+with open('HISTORY.md') as history_file:
+    HISTORY = history_file.read()
+
+setup_args = dict(
+    name='aristonremotethermo',
+    version='1.0.0',
+    description='Ariston NET Remotethermo integration',
+    long_description_content_type="text/markdown",
+    long_description=README + '\n\n' + HISTORY,
+    license='MIT',
+    packages=find_packages(),
+    author='Oleh Pashchuk',
+    author_email='chomu.nattsol@gmail.com',
+    keywords=['Ariston NET', 'Remotethermo', 'Ariston'],
+    url='https://github.com/chomupashchuk/ariston-remotethermo-api',
+    download_url='https://pypi.org/project/aristonremotethermo/'
+)
+
+install_requires = [
+    'requests'
+]
+
+if __name__ == '__main__':
+    setup(**setup_args, install_requires=install_requires)
