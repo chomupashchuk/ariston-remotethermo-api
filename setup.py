6890a6966866a2f04494ae37441ea76b4e84738d
chomupashchuk <pashchuk.oleg@gmail.com>
2021-01-31 18:25:29 +0200

    Logger updates and maximu temperature fetching for Lydos Hybrid

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('HISTORY.md') as history_file:
 
 setup_args = dict(
     name='aristonremotethermo',
-    version='1.0.28',
+    version='1.0.29',
     description='Ariston NET Remotethermo integration',
     long_description_content_type="text/markdown",
     long_description=README + '\n\n' + HISTORY,
